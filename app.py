0cdc6d1181a7cd85d7ec995c2351d05c7dc7213a
Dlang-max <langd052405@gmail.com>
2024-03-30 21:07:55 -0400

    added variable file

--- app.py
+++ app.py
@@ -8,9 +8,24 @@ def home():
 
 @app.route("/api/post-blog", methods=['POST'])
 def api_post_blog():
+    header = request.headers
     data = request.get_json()
-    print(data['title'])
-    return 'cool bean yo'
+    print(data['title'], flush=True)
+    print(header['Content-Type'], flush=True)
+    return 'got it'
+
+@app.route("/api/checkpoint", methods=['GET'])
+def get_checkpoint():
+    
+    with open('variable.txt', 'r') as f:
+        return f.read().strip()
+    
+@app.route("/api/set-checkpoint/<checkpoint>", methods=['POST'])
+def set_checkpoint(checkpoint):
+    
+    with open('variable.txt', 'w') as f:
+        f.write(str(checkpoint))
+    return checkpoint
 
 if(__name__ == 'main'):
     app.run()
